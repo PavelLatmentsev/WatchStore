1f7f74109a455eb95e55423b694fe8c7f8f6734e
Pavel <nightcharm@mail.ru>
2023-03-29 14:22:03 +0300

    add animation in advantage block

--- src/js/script.js
+++ src/js/script.js
@@ -57,30 +57,60 @@ $(document).ready(function () {
                     required: true,
                     minlength: 2
                 },
-                email: "required",
-                phone: {
-                    required: true, email: true
-                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
             },
             messages: {
                 name: {
-                    required: "Пожалуйста, введите имя",
-                    minlength: jQuery.validator.format("Введите {0} символов")
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
                 },
-                phone: "Пожалуйста, введите телефон",
+                phone: "Пожалуйста, введите свой номер телефона",
                 email: {
-                    required: "Пожалуйста, введите почту",
-                    email: " email должен быть по формату"
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
                 }
             }
-
-
-        })
-    }
+        });
+    };
     validateForms("#consultation-form");
     validateForms("#order form");
     validateForms("#consultation form");
-    $("input[name=phone]").mask("+ 7 (999) 999-999-99")
+    $("input[name=phone]").mask("+7 (999) 999-99-99")
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return
+        }
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $("consultation, #order").fadeOut();
+            $(".overlay, #thanks").fadeIn("slow");
+            $("form").trigger("reset");
+        });
+        return false;
+
+    })
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $(".pageup").fadeIn();
+        } else {
+            $(".pageup").fadeOut();
+        }
+    })
+    $("a[href^='#']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
 });
 const slider = tns({
     container: '.carousel__inner',
